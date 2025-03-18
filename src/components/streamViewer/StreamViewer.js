b00719b871354e3fd07efa6924b3363c8f41d1a7
Yuriy Shaklak <yurijfedor178@gmail.com>
2025-03-18 10:07:04 +0200

    update wss tunnel

--- src/components/streamViewer/StreamViewer.js
+++ src/components/streamViewer/StreamViewer.js
@@ -65,7 +65,7 @@ export const StreamViewer = () => {
   };
 
   const connectWebSocket = () => {
-    socket.current = new WebSocket("wss://3f69-91-218-88-220.ngrok-free.app");
+    socket.current = new WebSocket("wss://8325-91-218-88-220.ngrok-free.app");
 
     socket.current.onopen = () => {
       console.log("✅ WebSocket підключено");
